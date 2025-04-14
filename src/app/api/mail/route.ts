d48db6b1b53bfc83489e6331eba9ed1384880751
tobias <tobys312@gmail.com>
2025-04-14 14:58:43 -0300

    fix: remove unnecessary folders

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -2,7 +2,7 @@ import { render } from "@react-email/render";
 import Email from "@/../emails";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY as string);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   const { name, email, message } = await request.json();
